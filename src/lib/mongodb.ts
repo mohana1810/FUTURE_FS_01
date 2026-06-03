40b9368d2cf5098d4331eda72fc10ade50e87d56
Mohana Jarajapu <anier6180811@gmail.com>
2026-06-03 23:12:19 +0530

    Update mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,23 +1,18 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+let clientPromise: Promise<MongoClient> | null = null;
 
-if (!uri) {
-  throw new Error("Missing MONGODB_URI");
+export async function getMongoClient() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("Missing MONGODB_URI");
+  }
+
+  if (!clientPromise) {
+    const client = new MongoClient(uri);
+    clientPromise = client.connect();
+  }
+
+  return clientPromise;
 }
-
-declare global {
-  // eslint-disable-next-line no-var
-  var __mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-const client = new MongoClient(uri);
-
-const clientPromise =
-  global.__mongoClientPromise ?? client.connect();
-
-if (!global.__mongoClientPromise) {
-  global.__mongoClientPromise = clientPromise;
-}
-
-export default clientPromise;
